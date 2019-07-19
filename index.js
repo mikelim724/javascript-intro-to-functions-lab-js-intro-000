e53bf5421243a3b323c110bae01e92ecb8ecfcf5
imperative-cron-3751 <mikelim724@gmail.com>
2019-07-19 21:23:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,4 +17,10 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if(string.toLowerCase()===string)
   {return "I can't hear you!"}
+  else if (string.toUpperCase()===string) {
+    return "YES INDEED!"
+  }
+  else if ("I love you, Grandma."=== string) {
+    return "I love you, too."
+  }
 }
